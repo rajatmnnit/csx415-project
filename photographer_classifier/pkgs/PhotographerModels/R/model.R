914f8040a621e9f1ff1d8c96c6db916cec342b14
Rajat Jain <rajatj@adobe.com>
2018-05-08 15:13:14 -0700

    Updated Model package and documentation

--- photographer_classifier/pkgs/PhotographerModels/R/model.R
+++ photographer_classifier/pkgs/PhotographerModels/R/model.R
@@ -1,12 +1,12 @@
-
-
-#' Load the model corresponding to the given argument.
+#' Load the classification model.
 #'
-#' Takes in a model type. Valid model types are : linearsvm, gbm, ...
-#' @param mtype
-#' @return
+#' Loads a pre-trained persistent binary classification model to classify customers as "PHOTOGRAPHERS" or "OTHERS".
+#'
+#' @param mtype Type of the model to be returned. Valid value [svm | rf | gbm]. Defaults to the highest accuracy gbm model.
+#' @return a caret::train binary classification model object trained using specified model types.
 #' @export
-load.model <- function(mtype) {
-  model <- readRDS("/Users/rajatj/csx415/csx415-project/photographer_classifier/pkgs/PhotographerModels/data/linear_svm.rds")
+load.model <- function(mtype="gbm") {
+  model.location = paste0("/Users/rajatj/csx415/csx415-project/photographer_classifier/pkgs/PhotographerModels/data/", mtype, ".rds")
+  model <- readRDS(model.location)
   return(model)
 }
