c624ad5a051c00918e48c702168d213290cc3f06
Rajat Jain <rajatj@adobe.com>
2018-05-27 20:20:43 -0700

    Updated documentation for PhotographerScoring package

--- photographer_classifier/pkgs/PhotographerScoring/R/score.R
+++ photographer_classifier/pkgs/PhotographerScoring/R/score.R
@@ -5,9 +5,9 @@ library(caret)
 #'
 #' Classify new customers as "PHOTOGRAPHERS" or "OTHERS" by scorin against a pre-trained persistent binary classification model.
 #'
-#' @param data  New data with structure [].
-#' @param mtype Type of the model to be returned. Valid value [svm | rf | gbm]. Defaults to the highest accuracy gbm model.
-#' @return a caret::train binary classification model object trained using specified model types.
+#' @param data  Data frame containing new data with structure [member_guid, lr_cc_usage, lr_cl_usage, lr_mo_usage, storage_usage, ps_usage, stock_usage].
+#' @param mtype Type of the model to be used for classification. Valid value [svm | rf | gbm]. Defaults to the highest accuracy gbm model.
+#' @return a data frame of the format [member_guid, class], where "class" is the predicted class from model for the corresponding member_guid.
 #' @export
 classify <- function(data, mtype="gbm") {
   fit <- load.model(mtype);
