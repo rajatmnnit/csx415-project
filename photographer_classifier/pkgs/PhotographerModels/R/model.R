a5a6436c5abe4cf68c448c0fac3aff91b8bdaca8
Rajat Jain <rajatj@adobe.com>
2018-05-27 22:36:53 -0700

    Updated package documentation and organization

--- photographer_classifier/pkgs/PhotographerModels/R/model.R
+++ photographer_classifier/pkgs/PhotographerModels/R/model.R
@@ -3,10 +3,21 @@
 #' Loads a pre-trained persistent binary classification model to classify customers as "PHOTOGRAPHERS" or "OTHERS".
 #'
 #' @param mtype Type of the model to be returned. Valid value [svm | rf | gbm]. Defaults to the highest accuracy gbm model.
+#' 
 #' @return a caret::train binary classification model object trained using specified model types.
+#' 
+#' @examples 
+#'     load.model()
+#'     load.model(mtype="rf")
+#'     
+#' @import caret
+#' 
 #' @export
 load.model <- function(mtype="gbm") {
-  model.location = paste0("/Users/rajatj/csx415/csx415-project/photographer_classifier/pkgs/PhotographerModels/data/", mtype, ".rds")
+  # Find location of correct stored model file from data directory of the package.
+  model.location <- system.file("data", paste0(mtype,".rds"), package="PhotographerModels")
+  
+  # Read and return model from rds file.
   model <- readRDS(model.location)
   return(model)
 }
--- photographer_classifier/pkgs/PhotographerScoring/R/score.R
+++ photographer_classifier/pkgs/PhotographerScoring/R/score.R
@@ -1,17 +1,26 @@
-library(PhotographerModels)
-library(caret)
-
 #' Classify new data based on trained models.
 #'
-#' Classify new customers as "PHOTOGRAPHERS" or "OTHERS" by scorin against a pre-trained persistent binary classification model.
+#' Classify new customers as "PHOTOGRAPHERS" or "OTHERS" by scoring against a pre-trained persistent binary classification model.
 #'
 #' @param data  Data frame containing new data with structure [member_guid, lr_cc_usage, lr_cl_usage, lr_mo_usage, storage_usage, ps_usage, stock_usage].
 #' @param mtype Type of the model to be used for classification. Valid value [svm | rf | gbm]. Defaults to the highest accuracy gbm model.
+#' 
 #' @return a data frame of the format [member_guid, class], where "class" is the predicted class from model for the corresponding member_guid.
+#' 
+#' @examples 
+#'     classify(new.data)
+#'     classify(new.data, mtype="rf")
+#'     
+#' @import PhotographerModels
+#' 
 #' @export
 classify <- function(data, mtype="gbm") {
+  # Load the model from PhotographerModels based on the selected model type.
   fit <- load.model(mtype);
   
+  # Predict results for new data.
   data$class <- predict(fit, newdata=data, type="raw")
-  data[,c(1,8)]
+  
+  # Retuen only the member_guid, predicted class.
+  return(data[,c(1,8)])
 }
